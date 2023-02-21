7551a8991d7516fd445e5d82e4feb15c98c48a40
Nicholas Wilson <forzenacademy@gmail.com>
2023-02-20 17:44:56 -0800

    commit

---
+++ lottie-compose/build/generated/source/buildConfig/debug/com/airbnb/lottie/compose/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.airbnb.lottie.compose;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.airbnb.lottie.compose";
+  public static final String BUILD_TYPE = "debug";
+}
